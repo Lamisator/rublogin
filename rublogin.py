c016d738f522a4c240ca348b9ab37f088f0c3ecd
Lamisator <marius.kloeck@googlemail.com>
2015-08-11 09:29:41 +0200

    Added parameter handling, variable interval

--- rublogin.py
+++ rublogin.py
@@ -8,8 +8,8 @@ import subprocess
 import time
 import os
 import sys
+import getopt
 
-interval = 15
 verb = 0
 loginid = ""
 pw = ""
@@ -59,25 +59,35 @@ def establish_connection(user, pw):
 	else:
 		print("Fail.")
 		return 0
-def dbgmsg(msg):
-	if verb: print("Dbg: " + msg) 
-	return
-def main():
+def logout():
+	dbgmsg("Trying to log out...")
+	ip = get_ip_address()
+	if ip == 0:
+		print("Network is unreachable.\n")
+		return 0
+	payload = {'code': '1', 'loginid': '', 'password': '', 'ipaddr': ip, 'action': 'Logout'}
+	text = ""
 	try:
-		if(sys.argv[1] == "-v"):
-			global verb
-			verb = 1
-			print("Debugging messages enabled.")
+		dbgmsg("Trying to log out via HTTP-POST...")
+		r = requests.post("https://login.rz.ruhr-uni-bochum.de/cgi-bin/laklogin", data=payload)
+		text = r.text
 	except Exception:
-		 verb = 0
+		print("An error occured while trying to logout.\n")
+	if "erfolgreich" in text:
+		print("O.K., successfully logged out.")
+		return 1
+	else:
+		print("Fail.")
+		return 0
+
+def login(watchdog, interval):
 	global loginid, pw
 	loginid = input("Login ID: ") 
 	pw = getpass.getpass()
 	if establish_connection(loginid, pw) == 0:
 		exit()
-	watchdog = input ("Enable connection watchdog? (y/n) ")
-	wd_enabled = 1
-	if watchdog == 'y':
+	if watchdog:
+		wd_enabled = 1
 		try:
 			pid = os.fork()
 		except OSError:
@@ -95,5 +105,46 @@ def main():
 				print("\nRUBLOGIN-Watchdog: Connection lost. Trying to re-establish...")
 				establish_connection(loginid, pw)
 
-main()
+def dbgmsg(msg):
+	if verb: print("Dbg: " + msg) 
+	return
+
+def help():
+	print("Usage:\n -l / --logout : Terminates the current HIRN-Port-Session\n -w / --watchdog [interval]: Enables the watchdog. Checks the status of the connection every [interval] seconds and reconnects, if neccessary\n -v / --verbose : Prints debug messages throughout the login/logout process\n -h / --help : Displays this help")
+def main(argv):
+	global verb
+	logout_v = 0
+	watchdog = 0
+	interval = 0
+	try:
+		opts, args = getopt.getopt(argv, "hvlw:", ["help","verbose","logout","watchdog="])	
+	except Exception:
+		verb = 0
+		print("Invalid arguments. Use the -h Parameter if you are lost.")
+		sys.exit()
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			help()
+			sys.exit()
+		elif opt in("-v",  "--verbose"):
+			verb = 1
+			print("Debugging messages enabled.")
+		elif opt in ("-l", "--logout"):
+			logout_v = 1
+		elif opt in ("-w", "--watchdog"):
+			watchdog = 1
+			try:
+				interval = int(arg)
+			except ValueError:
+				print("Only integers, please. Aborting.")
+				sys.exit()
+			if interval < 5:
+				print("Interval must be 5 seconds or longer. Aborting.")
+				sys.exit()
+			print("Watchdog enabled.")
+	if logout_v:
+		logout()
+	else:
+		login(watchdog, interval)
+main(sys.argv[1:])
 
